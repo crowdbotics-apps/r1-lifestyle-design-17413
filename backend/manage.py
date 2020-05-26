eb79dc52f190983a96fc418226c424218dfb0308
Crowdbotics <team@crowdbotics.com>
2020-05-26 16:30:39 +0000

    Update models

--- backend/manage.py
+++ backend/manage.py
@@ -5,7 +5,9 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'r1_lifestyle_design_17413.settings')
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "r1_lifestyle_design_17413.settings"
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +19,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- backend/r1_lifestyle_design_17413/wsgi.py
+++ backend/r1_lifestyle_design_17413/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'r1_lifestyle_design_17413.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "r1_lifestyle_design_17413.settings")
 
 application = get_wsgi_application()
